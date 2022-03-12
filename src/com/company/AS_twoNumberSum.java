527c1c2c123aefad0db75d2b7004e4bf9f48fdbc
Marisa Shang <yutong.shang.vanderbilt@gmail.com>
2022-03-12 15:36:16 -0800

    03/12/2022

---
+++ src/com/company/AS_twoNumberSum.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AS_twoNumberSum {
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        Set<Integer> set= new HashSet<>();
+        for(int a:array){
+            int b=targetSum-a;
+            if(set.contains(b)){
+                //以下两行可以变成return new int[] {a,b}
+                int[] result={a,b};
+                return result;
+            }else{
+                set.add(a);
+            }
+        }
+        //int[0]注意
+        return new int[0];
+    }
+
+}
