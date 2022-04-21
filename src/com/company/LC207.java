3c64cb4fd868b3c7bdfc53b8acb485cb20ca5446
Marisa Shang <yutong.shang.vanderbilt@gmail.com>
2022-04-21 16:22:05 -0700

    Update LC207.java

--- src/com/company/LC207.java
+++ src/com/company/LC207.java
@@ -5,6 +5,16 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
+/*
+Time complexity is O(E + V)
+Because when we build the HashMap, it is O(E). We need to go through each edge to compute the hashmap
+The backtracking process takes O(V+E), since we implement DFS travasal in the graph
+Thus we visit each node and each edge only once in worst case.
+
+Space complexity is O(E+V)
+When we build the graph, it uses E+V space. During the backtracking process, the visited and checked array takes O(V) space
+Thus overall it takes O(E+V) space
+ */
 public class LC207 {
         public boolean canFinish(int numCourses, int[][] prerequisites) {
 
@@ -24,19 +34,20 @@ public class LC207 {
                 }
             }
 
-            //
+            //two boolean array to keep track of which node is visited and which is checked
             boolean[] checked = new boolean[numCourses];
-            boolean[] visted = new boolean[numCourses];
+            boolean[] visited = new boolean[numCourses];
 
-            for (int current = 0; current < numCourses; ++current) {
-                if (this.cycle(current, courseMap, checked, visted))
+
+            for(int current: courseMap.keySet()){
+                if (this.cycle(current, courseMap, checked, visited))
                     return false;
             }
             return true;
         }
 
         /*
-         * postorder DFS check that no cycle would be formed starting from current
+         * use DFS to check that there is no cycle from current
          */
         protected boolean cycle(Integer current,HashMap<Integer, List<Integer>> courseMap,
                 boolean[] checked, boolean[] visited) {
